1a2c6bfc82fa45fc3256e57069df03f19ee1fe15
Anuj Singh <anujsingh@Anujs-MacBook-Air.local>
2026-07-26 14:59:09 +0530

    Connect frontend with Render backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,6 +2,9 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: "https://vajra-api-gh6g.onrender.com",
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 export default api;
